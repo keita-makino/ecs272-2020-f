00413a4fbb2836ec61b3dee1c8555c4eb71aaa3e
keita-makino <kemakino@ucdavis.edu>
2020-03-20 23:36:05 -0700

    Updates

---
+++ precache-manifest.0bfb9369784cf60e2730e1f89ccc341c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91efaa717338b9773c18721bb4efef92",
+    "url": "/ecs272-2020-f/index.html"
+  },
+  {
+    "revision": "5d2592dbf0bd425ba695",
+    "url": "/ecs272-2020-f/static/css/main.1dffdf79.chunk.css"
+  },
+  {
+    "revision": "5ba4fe52cc78e6d9b4c0",
+    "url": "/ecs272-2020-f/static/js/2.b68edf1e.chunk.js"
+  },
+  {
+    "revision": "416fd97f8594a4822b607949bb022aaa",
+    "url": "/ecs272-2020-f/static/js/2.b68edf1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2592dbf0bd425ba695",
+    "url": "/ecs272-2020-f/static/js/main.1a24acf4.chunk.js"
+  },
+  {
+    "revision": "28a1dc20b85d8b447cd4",
+    "url": "/ecs272-2020-f/static/js/runtime-main.5a6e370a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecs272-2020-f/precache-manifest.39394940ef4a2968877419346bb7edee.js"
+  "/ecs272-2020-f/precache-manifest.0bfb9369784cf60e2730e1f89ccc341c.js"
 );
 
 self.addEventListener('message', (event) => {
